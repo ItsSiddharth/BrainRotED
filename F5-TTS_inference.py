60e4294c5d90b0aeafb69a73bd197615467e8b21
Siddharth <49035191+ItsSiddharth@users.noreply.github.com>
2025-06-25 00:28:27 +0930

    Add files via upload

---
+++ F5-TTS_inference.py
@@ -0,0 +1,4 @@
+f5-tts_infer-cli --model F5TTS_v1_Base \
+--ref_audio "/workspace/brain_rot_db/ronaldo/ronaldo.mp3" \
+--ref_text "Not really, we didnt play so good. But sometimes the football is like that, we have to play the way the team the other team show" \
+--gen_text "Hey I'm Ronaldo the greatest striker and goal scorer of all time. But today I'm here to teach you vectors and dot products and all the fun stuff related to that."
